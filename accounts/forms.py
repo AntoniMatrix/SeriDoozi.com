999562685db55f01296a4dbf84be76ebb2efa688
Antoni Matrix <Antoni.Matrix2003@gmail.com>
2026-07-06 12:46:55 +0330

    add first & last name filde to register form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 """
 Account-related forms.
 """
-
+import re
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
@@ -9,11 +9,21 @@ from .models import User
 
 class RegisterForm(UserCreationForm):
     """Register form extending UserCreationForm."""
-    email = forms.EmailField(required=True, label="ایمیل")
-    phone = forms.CharField(required=True, label="شماره موبایل")
-    first_name = forms.CharField(required=True , label="نام")
-    last_name = forms.CharField(required=True , label="نام خانوادگی")
+    email = forms.EmailField(required=True, label="ایمیل", widget=forms.TextInput(attrs={ "placeholder": "example@email.com" }))
+    phone = forms.CharField(required=True, label="شماره موبایل", widget=forms.TextInput(attrs={ "placeholder": "09xx xxx xxxx" }))
+    first_name = forms.CharField(required=True , label="نام", widget=forms.TextInput(attrs={ "placeholder": "مثلا: علی" }))
+    last_name = forms.CharField(required=True , label="نام خانوادگی", widget=forms.TextInput(attrs={ "placeholder": "مثلا: هاشمی" }))
 
     class Meta:
         model = User
         fields = ("first_name", "last_name", "username", "email",  "phone", "password1", "password2")
+
+        def clean_phone(self):
+            phone = self.cleaned_data["phone"]
+
+            if not re.fullmatch(r"09\d{9}", phone):
+                raise forms.ValidationError(
+                    "شماره موبایل باید با 09 شروع شده و 11 رقم باشد."
+                )
+
+            return phone
--- accounts/models.py
+++ accounts/models.py
@@ -5,9 +5,14 @@ We use AbstractUser and manage roles via Django Groups + Permissions.
 """
 
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
+iran_phone_validator = RegexValidator(
+    regex=r"^09\d{9}$",
+    message="شماره موبایل باید با 09 شروع شده و 11 رقم باشد."
+)
 
 class User(AbstractUser):
     """Custom User with optional phone field."""
-    phone = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=20, blank=True, unique=True, validators=[iran_phone_validator],)
