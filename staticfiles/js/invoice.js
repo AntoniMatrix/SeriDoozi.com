6f5cb13556c228c2083ef25c0bf73c7da76e9b58
Antoni Matrix <Antoni.Matrix2003@gmail.com>
2026-07-07 11:24:51 +0330

    update .gitignore

--- staticfiles/js/invoice.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.getElementById("print");
-    if (btn) {
-        btn.addEventListener("click", function () {
-            window.print();
-        });
-    }
-});
